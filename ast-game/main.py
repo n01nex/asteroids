abae803bf0643e362652076bd992d3418b00a0ce
n01nex <n01nex@proton.me>
2025-07-06 14:44:23 +0200

    drawing triangle player in the middle

--- ast-game/main.py
+++ ast-game/main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -17,6 +18,10 @@ def main():
     # Initializing screen display with set sizes
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Initializing the player object
+    p1 = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+
     while game_running:
 
         # Registering the closure of the window and exiting the game loop
@@ -26,10 +31,13 @@ def main():
         
         # fill the screen with black
         screen.fill("black")
-        # flip refresh the screen
+        # draws the player
+        p1.draw(screen)
+        # flip refreshes the screen
         pygame.display.flip()
-
+        # pauses the loop for 1/60th of a second and stores the value since it was called in milliseconds in dt
         dt = int(clock.tick(60))/1000
+
 
 
 
