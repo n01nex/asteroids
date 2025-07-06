a984054d984237656c29e88cae62b56e6633f773
n01nex <n01nex@proton.me>
2025-07-06 15:33:42 +0200

    group management and player movement

--- ast-game/main.py
+++ ast-game/main.py
@@ -11,6 +11,10 @@ def main():
     game_running = True
     pygame.init()
 
+    # group creation for screen update management
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     # FPS
     clock = pygame.time.Clock()
     dt = 0
@@ -18,9 +22,9 @@ def main():
     # Initializing screen display with set sizes
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # Initializing the player object
-    p1 = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
-
+    # Initializing the player object and setting group containers
+    Player.containers = (updatable, drawable)
+    p1 = Player(int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2))
 
     while game_running:
 
@@ -31,16 +35,15 @@ def main():
         
         # fill the screen with black
         screen.fill("black")
-        # draws the player
-        p1.draw(screen)
+        # draws the items of the group drawable
+        for drw in drawable:
+            drw.draw(screen)
+        # update the items after status change like key pressed
+        updatable.update(dt)
         # flip refreshes the screen
         pygame.display.flip()
         # pauses the loop for 1/60th of a second and stores the value since it was called in milliseconds in dt
         dt = int(clock.tick(60))/1000
-
-
-
-
 
 
 
